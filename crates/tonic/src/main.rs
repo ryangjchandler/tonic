19dfbcb8b242a54187886944aad3dbba77aaf1db
Ryan Chandler <ryangjchandler@gmail.com>
2021-11-22 11:31:22 +0000

    feature: basic repl

--- crates/tonic/src/main.rs
+++ crates/tonic/src/main.rs
@@ -1,5 +1,6 @@
 use tonic_compiler::compile;
 use rquickjs::{Runtime, Context, Func, Value, Rest};
+use rustyline::{Editor, error::ReadlineError};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -7,7 +8,7 @@ struct Cli {
     #[structopt(long = "debug", short = "d", help = "Output debug information (JS, memory usage, etc)")]
     debug: bool,
 
-    file: String,
+    file: Option<String>,
 }
 
 pub fn println(vs: Rest<Value>) {
@@ -15,21 +16,14 @@ pub fn println(vs: Rest<Value>) {
         println!("{}", match true {
             _ if v.is_string() => v.into_string().unwrap().to_string().unwrap(),
             _ if v.is_number() => v.as_number().unwrap().to_string(),
+            _ if v.is_bool() => v.as_bool().unwrap().to_string(),
             _ => unimplemented!(),
         });
-    } 
+    }
 }
 
 fn main() {
     let args = Cli::from_args();
-
-    let contents = read(args.file);
-    let compiled = compile(&contents[..]);
-
-    if args.debug {
-        println!("=== JS OUTPUT ===");
-        println!("{}", compiled);
-    }
 
     let runtime: Runtime = Runtime::new().unwrap();
     runtime.set_max_stack_size(256 * 2048);
@@ -40,17 +34,55 @@ fn main() {
         println!("=== EVAL ===");
     }
     
-    context.with(|ctx: rquickjs::Ctx| {
-        let glob = ctx.globals();
+    if let Some(file) = args.file {
+        let contents = read(file);
+        let compiled = compile(&contents[..]);
+    
+        if args.debug {
+            println!("=== JS OUTPUT ===");
+            println!("{}", compiled);
+        }
 
-        glob.set("println", Func::from(println)).unwrap();
+        context.with(|ctx: rquickjs::Ctx| {
+            let glob = ctx.globals();
+    
+            glob.set("println", Func::from(println)).unwrap();
+    
+            ctx.eval::<(), _>(compiled).unwrap();
+        });
+    
+        if args.debug {
+            println!("=== DEBUG ===");
+            println!("Memory used (bytes): {}", runtime.memory_usage().memory_used_size);
+        }
+    } else {
+        let mut rl = Editor::<()>::new();
 
-        ctx.eval::<(), _>(compiled).unwrap();
-    });
+        loop {
+            let line = rl.readline(">> ");
 
-    if args.debug {
-        println!("=== DEBUG ===");
-        println!("Memory used (bytes): {}", runtime.memory_usage().memory_used_size);
+            match line {
+                Ok(line) => {
+                    rl.add_history_entry(line.as_str());
+
+                    context.with(|ctx: rquickjs::Ctx| {
+                        let glob = ctx.globals();
+                
+                        glob.set("println", Func::from(println)).unwrap();
+                
+                        ctx.eval::<(), _>(line).unwrap();
+                    });
+                },
+                Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
+                    println!("Exiting!");
+                    break
+                },
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                    break
+                }
+            }
+        }
     }
 }
 
