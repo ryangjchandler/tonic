bf449690efa9f14e189617279fb7623907e2a615
Ryan Chandler <ryangjchandler@gmail.com>
2021-11-26 20:40:43 +0000

    feature(parser): hoist imports

--- crates/tonic-parser/src/passes/mod.rs
+++ crates/tonic-parser/src/passes/mod.rs
@@ -3,10 +3,19 @@ use std::cmp::Ordering;
 
 pub fn pass(ast: &mut Vec<Statement>) {
     hoist_functions(ast);
+    hoist_imports(ast);
 }
 
 fn hoist_functions(ast: &mut Vec<Statement>) {
     ast.sort_unstable_by(|a, _| if matches!(a, Statement::Function { .. }) {
+        Ordering::Less
+    } else {
+        Ordering::Equal
+    });
+}
+
+fn hoist_imports(ast: &mut Vec<Statement>) {
+    ast.sort_unstable_by(|a, _| if matches!(a, Statement::Pub { .. }) {
         Ordering::Less
     } else {
         Ordering::Equal
